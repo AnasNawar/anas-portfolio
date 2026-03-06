cbb5b7840eda80403e6fbc4324f36e5c73e224b9
Anas Nawar <anashazem266@gmail.com>
2026-03-06 23:58:47 +0200

    gdgdg

--- script.js
+++ script.js
@@ -21,7 +21,6 @@
 document.addEventListener("mousemove", (e) => {
   const flutter = document.querySelector(".flutter-logo");
   const dart = document.querySelector(".dart-logo");
-  if (!flutter || !dart) return;
   const x = e.clientX / window.innerWidth;
   const y = e.clientY / window.innerHeight;
   flutter.style.transform = `translate(${x * 60}px, ${y * 60}px)`;
